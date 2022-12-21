5f0a7982e80802b06d9b67119ae2b13b45200457
Filipe Augusto Santos Chaves <faugusto.chaves@gmail.com>
2022-12-21 17:36:54 -0300

    propriedades

---
+++ Data/Mappings/CategoryMap.cs
@@ -0,0 +1,36 @@
+using blog.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace blog.Data.Mappings;
+
+public class CategoryMap : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+     //FLUENT MAPPING
+      builder.ToTable("Category");
+      //Chave primaria
+      builder.HasKey(x => x.Id);
+        //identity
+      builder.Property(x => x.Id).ValueGeneratedOnAdd()
+        .UseIdentityColumn(); // identity 
+
+        //propriedades
+        builder.Property(x => x.Name)
+        .IsRequired()
+        .HasColumnName("Name")
+        .HasColumnType("NVARCHAR")
+        .HasMaxLength(80);
+
+        builder.Property(x => x.Slug)
+        .IsRequired()
+        .HasColumnName("Slug")
+        .HasColumnType("VARCHAR")
+        .HasMaxLength(80);
+
+
+
+
+    }
+}
